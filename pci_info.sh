1d80286f79daf8da1b8fd49c5517b5f69a55dd34
Christopher Ortiz Aldana <aldana.ortizch@gmail.com>
2025-05-25 17:22:15 -0700

    Works with HP/Dell

--- pci_info.sh
+++ pci_info.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
- 
-echo -e "\e[32m\t\tPCI Slot Information\e[0m\n"
 
 dmidecode -t slot | \
 
 awk '/Designation:/,/Bus Address:/' | \
 
-grep -vE "Characteristics:|ID:|Length:|3.3|SMB|PME|5.0 V" | \
+grep -vE "Characteristics:|ID:|Length:|3.3|SMB|PME|5.0 V|Data|Handle|System" | \
 
-awk '1; NR % 5 == 0 {print ""}'
+awk '1; NR % 4 == 0 {print ""}' 
