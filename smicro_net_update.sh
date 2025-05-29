3dfa48318a4ac8b7135c494df2aa70e1392f03e1
Christopher Ortiz Aldana <123458205+cortizaldana@users.noreply.github.com>
2025-05-29 14:21:58 -0700

    Create smicro_net_update.sh

---
+++ smicro_net_update.sh
@@ -0,0 +1,16 @@
+IPMI_USER=" "
+IPMI_PASS=" "
+TOOL="$1"
+IPADDR="$2"
+FILE="$3"
+
+case $TOOL in
+
+bios)
+$HOME/sum -i "$IPADDR" -u "$IPMI_USER" -p "$IPMI_PASS" -c UpdateBIOS --file "$FILE" ;;
+
+ipmi)
+$HOME/sum -i "$IPADDR" -u "$IPMI_USER" -p "$IPMI_PASS" -c UpdateBMC --file "$FILE" ;;
+
+*)
+echo "./smicro_net_update.sh bios|ipmi <IP> file"
